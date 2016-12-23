306f1973279bb792df23231b185afc71187a3707
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-23 11:33:01 +0100

    Added test for `ContextInterface`

---
+++ test/functional/ContextInterfaceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Dhii\Espresso\Test;
+
+/**
+ * Tests {@see \Dhii\Espresso\ContextInterface}.
+ *
+ * @since [*next-version*]
+ */
+class ContextInterfaceTest extends \Xpmock\TestCase
+{
+
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = '\\Dhii\\Espresso\\ContextInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \Dhii\Espresso\ContextInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getValue()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @covers \Dhii\Espresso\ContextInterface
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+    }
+
+}
