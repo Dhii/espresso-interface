a909477d6fac9dedbe02c017e835fefdcab88690
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-23 11:32:55 +0100

    Added test for `ExpressionInterface`

---
+++ test/functional/ExpressionInterfaceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dhii\Espresso\Test;
+
+/**
+ * Tests {@see \Dhii\ExpressionInterface}.
+ *
+ * @since [*next-version*]
+ */
+class ExpressionInterfaceTest extends \Xpmock\TestCase
+{
+
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = '\\Dhii\\Espresso\\ExpressionInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \Dhii\Espresso\ExpressionInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getTerms()
+            ->isNegated()
+            ->evaluate()
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @covers \Dhii\Espresso\ExpressionInterface
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+    }
+
+}
