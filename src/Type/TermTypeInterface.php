97e4547ba9b0fb750b10207c38f3ea602a050643
Miguel Muscat <miguelmuscat93@gmail.com>
2018-01-09 13:15:19 +0100

    Added `TermTypeInterface`

---
+++ src/Type/TermTypeInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dhii\Expression\Type;
+
+use Dhii\Expression\LogicalExpressionInterface;
+
+/**
+ * All types for expressions and terms.
+ *
+ * @since [*next-version*]
+ */
+interface TermTypeInterface extends
+    ArithmeticTypeInterface,
+    LogicalExpressionInterface
+{
+}
