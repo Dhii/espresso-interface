393ad51afc748204ea03255edbb65e05d70486a9
Miguel Muscat <miguelmuscat93@gmail.com>
2018-01-09 13:15:03 +0100

    Added `LogicalTypeInterface`

---
+++ src/Type/LogicalTypeInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dhii\Expression\Type;
+
+/**
+ * Logical types for expressions and terms.
+ *
+ * @since [*next-version*]
+ */
+interface LogicalTypeInterface extends
+    RelationalTypeInterface,
+    BooleanTypeInterface
+{
+}
